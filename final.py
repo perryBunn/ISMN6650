e3ca0f05ef7b13bfbcb9674296bf24c8f31abc4d
perryBunn <eagle4ever20@gmail.com>
2020-11-29 22:59:54 -0600

    Working on final

--- final.py
+++ final.py
@@ -24,21 +24,18 @@ def main(arguments):
     print("\tquartile : " + str(data['quantity'].quantile()))
 
     # I dont think that we need to filter this at all...
-    customer_data = data.filter(items=["name", "category", "quantity", "unit price", "ext price"])
-    dfclean = customer_data.groupby(['name'], dropna=False).sum('quantity')
-    print(dfclean)
+    customer_data = data.filter(items=["name", "category", "quantity", "unit price", "ext price", "date"])
+    total_sales = customer_data.groupby(['name'])["ext price"].sum()
+    total_sales.plot.bar()
+    plt.show()
 
-    labels = customer_data['name']
-    sales = customer_data.groupby(['name']).sum('quantity')['quantity']
-    width = .35
-    print("sales:\n" + str(sales))
-    # fig, ax = plt.subplots()
-    # ax.bar(labels, sales['quantity'], width)
-    # ax.set_ylabel('quantity sold')
-    # ax.set_title('Sales by Name')
-    # plt.show()
-    plt.figure()
-    dfclean.plot(kind='bar', stacked=True)
+    customer_data.groupby(['name', 'category'])["ext price"].sum().unstack(1).plot.bar(stacked=True)
+    plt.show()
+
+    # I have no fucking idea what im suppose to do with this histogram
+    customer_data["quantity"].plot.hist(density=True)
+    plt.show()
+
 
 
 
